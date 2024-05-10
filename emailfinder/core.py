b4d3f1484c92664f38206a6a084b6f036690a2e7
Evil2997 <dima.molchanow2012@gmail.com>
2024-05-10 13:45:42 +0300

    Changed the output to be added to the json file

--- emailfinder/core.py
+++ emailfinder/core.py
@@ -1,3 +1,5 @@
+import time
+
 from emailfinder.utils.finder import google
 from emailfinder.utils.finder import bing
 from emailfinder.utils.finder import baidu
@@ -5,13 +7,15 @@ from emailfinder.utils.finder import yandex
 from emailfinder.utils.color_print import print_error, print_ok
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-
 SEARCH_ENGINES_METHODS = {
-        "google": google.search,
-        "bing": bing.search,
-        "baidu": baidu.search,
-        "yandex": yandex.search
+    "google": google.search,
+    "bing": bing.search,
+    "baidu": baidu.search,
+    "yandex": yandex.search
 }
+
+data_emails = []
+data_list = []
 
 
 def _search(engine, target):
@@ -23,29 +27,30 @@ def _search(engine, target):
     except Exception as ex:
         print_error(f"{engine} error {ex}")
     return emails
-  
+
+
 def _get_emails(target):
     threads = 4
     emails = set()
     with ThreadPoolExecutor(max_workers=threads) as executor:
         future_emails = {executor.submit(_search, engine, target): engine for engine in SEARCH_ENGINES_METHODS.keys()}
-        for future in as_completed(future_emails):
+        for i, future in enumerate(as_completed(future_emails)):
             try:
+                searcher_list = ['bing', 'google', 'baidu', 'yandex']
                 data = future.result()
+                print(f"{searcher_list[i]}: {data}")
+                for value in data:
+                    data_result = {
+                        f"Find_in_{searcher_list[i]}": value
+                    }
+                    data_list.append(data_result)
                 if data:
                     emails = emails.union(data)
-            except Exception as ex:
-                print_error(f"Error: {ex}")
-    return list(emails)
+            except Exception as e:
+                print_error(f"Error: {e}")
+    return data_list
+
 
 def processing(target):
-    emails = _get_emails(target)
-    total_emails = len(emails)
-    emails_msg = f"\nTotal emails: {total_emails}"
-    print(emails_msg)
-    print("-" * len(emails_msg))
-    if total_emails > 0:
-        for email in emails:
-            print(email)
-    else:
-        print("0 emails :(. Closing...")
+    data_emails = _get_emails(target)
+    return data_emails
