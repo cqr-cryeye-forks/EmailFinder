8c626e248b128cb97173b6c3947918a6157fb5fa
Evil2997 <dima.molchanow2012@gmail.com>
2024-05-10 14:51:05 +0300

    temp

--- emailfinder/cli.py
+++ emailfinder/cli.py
@@ -17,14 +17,15 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--domain', help="Domain to search", required=True)
     # parser.add_argument('--version', help="Show Emailfinder version", action='version', version=__version__)
-    parser.add_argument('--output', help="Output-file to save result in json format. Example='data.json'")
+    parser.add_argument('--output', help="Output-file to save result in json format. Example='data.json'", required=True)
     args = parser.parse_args()
 
     output: str = args.output
+    target: str = args.domain
     MAIN_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parent
     output_json: str = MAIN_DIR / output
 
-    all_data = processing(args.domain)
+    all_data = processing(target)
     if all_data == []:
         all_data = {
             "Error": "Nothing found in EmailFinder"
